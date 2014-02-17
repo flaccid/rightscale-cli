2070e6020e9687182eb4e2217e44945d26cf8cd1
Chris Fordham <chris@xhost.com.au>
2014-02-17 22:04:03 +1100

    Enhance the configure lib to write back to the configure file, with subcommands for each directive.

--- lib/rightscale_cli/configure.rb
+++ lib/rightscale_cli/configure.rb
@@ -16,8 +16,12 @@
 
 require 'thor'
 require 'yaml'
+require 'rightscale_cli/config'
 require 'rightscale_cli/logger'
 require 'rightscale_cli/client'
+require 'ask_pass'
+require 'erb'
+require "base64"
 
 class RightScaleCLI
   class Configure < Thor
@@ -26,23 +30,65 @@ class RightScaleCLI
     def initialize(*args)
       super
       @logger = RightScaleCLI::Logger.new()
+      @conf_template_path = File.join(File.dirname(__FILE__), '..', 'templates', '/right_api_client.yml.erb')
+      @config = RightScaleCLI::Config.new.local
     end
+
+    no_commands{
+    def update_conf(directives)
+      renderer = ERB.new(IO.read(@conf_template_path))
+      File.open(local_filename, 'w') {|f| f.write(renderer.result(binding)) }
+    end
+    }
 
     desc "account", "Configure the RightScale account ID for the client."
     def account()
-
+      account = ask("RightScale account ID (e.g. 1337):") 
     end
 
     desc "user", "Configure the RightScale user for the client."
     def user()
-
+      email = ask("RightScale username (e.g. bill.gates@microsoft.com):")
     end
 
     desc "password", "Configure the RightScale user password for the client."
     def password()
-
+      password = ask_pass.strip
     end
-    
+
+    desc "api", "Configure the RightScale API version used by the client."
+    def api()
+      api_version = ask("RightScale API version (e.g. 1.5):")
+    end
+
+    desc "shard", "Configure the RightScale shard used by the client."
+    def shard()
+      api_url = "https://#{ask("RightScale shard (e.g. us-4.rightscale.com):")}"
+    end
+
+    desc "show", "Print the current configuration from ~/.rightscale/right_api_client.yml."
+    def show()
+      puts @config
+    end
+
+    desc "all", "Configure RightScale CLI."
+    def all()
+      directives = {
+        :account_id => account(),
+        :email => user(),
+        :password_base64 => Base64.encode64(ask_pass).strip,
+        :api_url => shard(),
+        :api_version => api()
+      }
+      @logger.debug(directives)
+
+      update_conf(directives)
+
+      puts 'Configuration updated.'
+    end
+
+    #default_task :all
+
     def self.banner(task, namespace = true, subcommand = false)
       "#{basename} #{task.formatted_usage(self, true, subcommand)}"
     end
