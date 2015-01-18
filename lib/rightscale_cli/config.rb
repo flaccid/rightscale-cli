d6544538f932c799408b906177a0619c09d90e5f
Chris Fordham <chris.fordham@industrieit.com>
2015-01-19 10:39:04 +1100

    Config class to support oauth token auth.

--- lib/rightscale_cli/config.rb
+++ lib/rightscale_cli/config.rb
@@ -1,5 +1,5 @@
 # Author:: Chris Fordham (<chris@fordham-nagy.id.au>)
-# Copyright:: Copyright (c) 2013 Chris Fordham
+# Copyright:: Copyright (c) 2013-2105 Chris Fordham
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,27 +18,32 @@ require 'yaml'
 require 'fileutils'
 
 class RightScaleCLI
+  # Represents a RightScale CLI configuration
   class Config
     attr_accessor :template_path, :config_home, :config_path, :directives
 
-    def initialize(*args)
-      @template_path = File.join(File.dirname(__FILE__), '..', 'templates', 'right_api_client.yml.erb')
+    def initialize(*)
+      @template_path = File.join(File.dirname(__FILE__),
+                                 '..', 'templates', 'right_api_client.yml.erb')
       @config_home = File.join(ENV['HOME'], '.rightscale')
       @config_path = File.join(@config_home, 'right_api_client.yml')
 
-      Dir.mkdir(@config_home) unless File.exists?(@config_home)
+      Dir.mkdir(@config_home) unless File.exist?(@config_home)
       FileUtils.touch(@config_path)
-      
+
       # write a fresh file if it does not load/parse
       unless YAML.load_file(@config_path)
         @directives = {
-           :account_id => '',
-           :email => '',
-           :password_base64 => '',
-           :api_url => 'https://us-4.rightscale.com',
-           :api_version => '1.5'
+          account_id: '',
+          email: nil,
+          password_base64: '',
+          access_token: '',
+          api_url: 'https://us-4.rightscale.com',
+          api_version: '1.5'
         }
-        File.open(@config_path, 'w') {|f| f.write(ERB.new(IO.read(@template_path)).result(binding)) }
+        File.open(@config_path, 'w') do |f|
+          f.write(ERB.new(IO.read(@template_path)).result(binding))
+        end
       end
 
       # load/reload the directives from the file
