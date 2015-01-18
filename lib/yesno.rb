25d228d38d28f4d904554fabc8a4e939626a0a1d
Chris Fordham <chris.fordham@industrieit.com>
2015-01-19 10:37:46 +1100

    Make yesno comply to Rubocop.

--- lib/yesno.rb
+++ lib/yesno.rb
@@ -16,16 +16,12 @@
 
 def yesno
   begin
-    system("stty raw -echo")
+    system('stty raw -echo')
     str = STDIN.getc
   ensure
-    system("stty -raw echo")
+    system('stty -raw echo')
   end
-  if str.downcase == "y"
-    return true
-  elsif str.downcase == "n"
-    return false
-  else
-    raise "Invalid response. Please enter y/n."
+  { 'y' => true, 'n' => false }.fetch(str.downcase) do
+    fail 'Invalid response. Please enter y/n.'
   end
 end
